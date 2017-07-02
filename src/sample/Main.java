1a4b70efa1a8277fbe81d93839177b006f7c9972
Ireneusz <ajron96@gmail.com>
2017-07-02 19:32:47 +0200

    builder pattern

---
+++ src/sample/Main.java
@@ -0,0 +1,37 @@
+package sample;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        User users[] = new User[3];
+
+        //USER 1
+        users[0] = new User.Builder("Ireneusz","Lewczuk").build();
+
+        //USER 2
+        users[1] = new User.Builder("Ireneusz", "Lewczuk")
+                            .age(21)
+                            .city("Ostroleka")
+                            .build();
+
+        //USER 3
+        users[2] = new User.Builder("Ireneusz","Lewczuk")
+                            .age(21)
+                            .phone("123456789")
+                            .city("Ostroleka")
+                            .address("Zyndrama St")
+                            .build();
+
+        int i = 1;
+
+        //show users's info
+        for(User user : users){
+            System.out.println("USER " + i);
+            user.info();
+            i++;
+        }
+
+
+    }
+}
---
+++ src/sample/User.java
@@ -0,0 +1,70 @@
+package sample;
+
+
+public class User {
+
+    private String firstName;   //required
+    private String lastName;    //required
+    private int age;            //optional
+    private String phone;       //optional
+    private String city;        //optional
+    private String address;     //optional
+
+    public User(Builder builder){
+        this.firstName = builder.firstName;
+        this.lastName = builder.lastName;
+        this.age = builder.age ;
+        this.phone = builder.phone;
+        this.city = builder.city;
+        this.address = builder.address;
+    }
+
+    public void info(){
+        System.out.println("Firstname: " + firstName);
+        System.out.println("Lastname: " + lastName);
+        System.out.println("Age: " + age);
+        System.out.println("Phone: " + phone);
+        System.out.println("City: " + city);
+        System.out.println("Address: " + address + '\n');
+    }
+
+    //Builder
+    public static class Builder {
+        private final String firstName;
+        private final String lastName;
+        private int age;
+        private String phone;
+        private String city;
+        private String address;
+
+        public Builder(String firstName, String lastName){
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+
+        public Builder age(int age){
+            this.age = age;
+            return this;
+        }
+
+        public Builder phone(String phone){
+            this.phone = phone;
+            return this;
+        }
+
+        public Builder city(String city){
+            this.city = city;
+            return this;
+        }
+
+        public Builder address(String address){
+            this.address = address;
+            return this;
+        }
+
+        public User build(){
+            return new User(this);
+        }
+    }
+
+}
